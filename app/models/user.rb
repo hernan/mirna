fada2e97284b82c261fc050d7287fc54ea01e3a9
Hernan <hernanjx@gmail.com>
2024-06-15 23:47:17 -0300

    Add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  scope :actives, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+  scope :admins, -> { where(admin: true) }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without name" do
+    user = User.new
+
+    assert_not user.save
+    assert user.errors.added?(:name, :blank)
+  end
+
+  test "should not save user without email" do
+    user = User.new
+
+    assert_not user.save
+    assert user.errors.added?(:email, :blank)
+  end
+
+  test "filter active/inactive users" do
+    User.create!(
+      [
+        { name: "John Doe", email: "jdoe@example.com", active: true },
+        { name: "Jane Doe", email: "jane_doe@example.com", active: false },
+      ]
+    )
+
+    assert_equal 1, User.actives.count
+    assert_equal 1, User.inactive.count
+    assert_equal 'John Doe', User.actives.first.name
+    assert_equal 'Jane Doe', User.inactive.first.name
+  end
+
+  test "filter admin users" do
+    User.create!(
+      [
+        { name: "John Doe", email: "jdoe@example.com", admin: true },
+        { name: "Jane Doe", email: "jane_doe@example.com", admin: false },
+      ]
+    )
+
+    assert_equal 1, User.admins.count
+    assert_equal 'John Doe', User.admins.first.name
+  end
 end
