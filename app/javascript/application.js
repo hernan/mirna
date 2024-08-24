ccfb81db88739ea7095de100b4089454c2e4e4f5
Hernan <hernanjx@gmail.com>
2024-08-24 19:57:30 -0300

    Change default Rails confirm dialog.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+Turbo.setConfirmMethod((message, element) => {
+    let dialog = document.getElementById('turbo-confirm');
+    dialog.querySelector('.dialog-message').textContent = message;
+    dialog.showModal();
+
+    return new Promise((resolve, _reject) => {
+        dialog.addEventListener('close', (_event) => {
+            resolve(dialog.returnValue === 'confirm');
+        }, { once: true });
+    });
+});
